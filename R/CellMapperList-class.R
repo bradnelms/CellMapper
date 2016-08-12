ef5ee5c651adb74fd2e1fa090538c884e7968fa3
bradnelms <bnelms.research@gmail.com>
2016-08-12 16:45:26 -0700

    address reviewer comments

--- R/CellMapperList-class.R
+++ R/CellMapperList-class.R
@@ -3,7 +3,8 @@
 setValidity("CellMapperList", function(object) {
     valid <- TRUE
     if (identical(sort(names(object)), sort(c("B", "d")))) {
-        if (!(is.matrix(object$B) & is.numeric(object$B) & is.vector(object$d) & is.numeric(object$d))) {
+        if (!(is.matrix(object$B) & is.numeric(object$B) & is.vector(object$d) &
+		 is.numeric(object$d))) {
             valid <- "'B' must be numeric matrix and 'd' must be numeric vector"
         } else if (length(object$d) != dim(object$B)[1]) {
             valid <- "The length of 'd' must equal the number of rows in 'B'"
@@ -11,7 +12,8 @@ setValidity("CellMapperList", function(object) {
     } else {
         valid <- "'B' and 'd' must be present"
     }
-    if (!is.list(metadata(object))) { valid <- "metadata must be provided as a list" }
+    if (!is.list(metadata(object))) { valid <- paste0("metadata must be provid",
+	"ed as a list") }
     valid
 })
 
@@ -19,16 +21,28 @@ setMethod("show", "CellMapperList",
 	function(object)
 	{
 		meta = metadata(object)
-		metaValid = if (is.null(names(meta))) { FALSE } else { identical(sort(names(meta)), sort(c("nrow", "ncol", "DataSource", "GeneIDType"))) }
+		metaValid = if (is.null(names(meta))) { FALSE } else { 
+			identical(sort(names(meta)), sort(c("nrow", "ncol",
+				"DataSource", "GeneIDType"))) }
 		cat("An object of class \"", class(object), "\"\n",
-		"#  Provide as input to the 'CM.search' function of the 'CellMapper' package\n",
+		"#  Provide as input to the 'CMsearch' function of the",
+		" 'CellMapper' package\n",
 		if (metaValid) {
-			paste0("#  Derived from an expression dataset with ", meta$nrow, " genes and ", meta$ncol, " samples\n",
-			"#     Dataset source:  '", if (meta$DataSource == "") { "not provided" } else { meta$DataSource }, "'\n",
-			"#  The type of gene ID used is: '", if (meta$GeneIDType == "") { "unknown" } else { meta$GeneIDType }, "'\n",
-			"#     Example gene IDs:  '", paste(head(colnames(object$B)), collapse = "', '"), "', ...\n")
+			paste0("#  Derived from an expression dataset with ",
+			     meta$nrow, " genes and ", meta$ncol, " samples\n",
+			"#     Dataset source:  '", if (meta$DataSource == "") {
+			     "not provided" } else { meta$DataSource }, "'\n",
+			"#  The type of gene ID used is: '",
+			if (meta$GeneIDType == "") { "unknown" } else {
+				meta$GeneIDType }, "'\n",
+			"#     Example gene IDs:  '",
+			paste(head(colnames(object$B)), collapse = "', '"),
+			"', ...\n")
 		} else { "#  No metadata available\n" }, sep = '')
-		if (!metaValid) { warning("this object was not prepared with the 'CM.prep' function") }
+		if (!metaValid) {
+			warning("This object was not prepared with the",
+			" 'CMprep' function")
+		}
 	}
 )
 
